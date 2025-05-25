9ad7ba959980b2b09ee61aafa69fc6ead3997ec7
sanggitsaaran <sanggitkovai@gmail.com>
2025-05-25 09:09:53 +0530

    download orders done

--- knft/transferFile.php
+++ knft/transferFile.php
@@ -1,10 +1,10 @@
 <?php
 require('header.php');
 
-// Database Connection
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
-    die("❌ Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 // Fetch Product Details
